1c0a90e4259c83f356ccd42eed6580753de9a11f
gautamchowdhry <gchowdhry@gmail.com>
2015-12-17 09:45:52 -0500

    Update

--- clarence_girls_softball.js
+++ clarence_girls_softball.js
@@ -1,16 +1,16 @@
- 
  $(document).ready(function() {  
   var url = $(location).attr('href');  
   
+  
+   
+    
     if(url.indexOf('/registration?bid=') > -1){ 
-     
-         
       var user_id = $('#lapi-user-id').val();
 
       if (user_id) {
         var headers = {
           'accept': 'application/json',
-          'x-api-key': 'ae18f3e44c48a594dc49c2bbc7608b2e'
+          'x-api-key': '40a1ccc8f7f77b065884a30439839d96'
           };
 
         var site_name = $('#lapi-site-name').val();
@@ -19,33 +19,42 @@
       
         $.ajax(site_url + '/users/' + user_id + '/registrations', {
           dataType: 'json',
-          cache: false, 
+          cache:false,
           headers: headers
         }).done(function (data) {
-                      
-            var regCount = 0;            
-                        
+        
+            var regCount = 0;
             for (var i = 0, len = data.length; i < len; i++) {
               var reg = data[i];                            
-              if (reg.programState != 'COMPLETED' && reg.userType == 'CHILD') {
-                regCount++;
+              if (reg.programState != 'COMPLETED' && reg.userType == 'CHILD' &&  reg.programType == 'LEAGUE') {                
+                regCount++;                
               }
            }
-            
-            var msgText;
-            
-            // 3rd and Higher is discounted
-            if (regCount >= 2) {
-                msgText  = 'Congratulations. You qualify for our <strong>multi child discount</strong>.  This registration will be discounted.'
-                $("h2:contains('Discount Code')").after('<div class="participant-headsup">' + msgText + '</span>'); //desktop
-                $("h3:contains('Discount Code')").after('<div class="alert">' + msgText + '</span>'); //mobile
-                $('#discountCode').val('3rdsibling');
-                $("#discountCode").attr('readonly', true);
-                $("#discountCode").attr("style", "background: #CCC; color: #333;border: 1px solid #666");
+
+  
+            var pageTitle = ''
+            if($('#is-mobile').length){
+               // mobile
+               pageTitle = $('#up-front-payment-details').text();
+            }     
+            else {
+                //desktop
+                pageTitle = $('[data-id="reg-terms"]').text();
             }
-            
-        
            
+            
+            if (pageTitle.toLowerCase().indexOf("league") > -1) {
+            
+                if (regCount >= 2) {
+                console.log('3');
+                  var newText = 'Congratulations. You qualify for our <strong>multi player sibling discount</strong>.  You\'ll <strong>save 50%</strong> on this registration.'
+                  $('[data-id="discount-code"]').after('<div class="participant-headsup">' +newText + '</span>');
+                  $('#discountCode').val('3rd50off');
+                  $("#discountCode").attr('readonly', true);
+                  $("#discountCode").attr("style", "background: #CCC; color: #333;border: 1px solid #666");                       
+                }
+            
+           }
             
           }).fail(function (data) {
             // I guess we don't really care if it fails
